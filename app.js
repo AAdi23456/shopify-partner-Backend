16997dd3a711bbbc8612f2f0d828a318f4fdd886
AAdi23456 <118712556+AAdi23456@users.noreply.github.com>
2024-04-07 13:46:54 +0530

    added cors

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 const logger = require('./logger'); 
 const routes = require('./routes/index');
+const cors=require("cors")
 
 app.use(express.json());
-
+app.use(cors());
 
 app.use((req, res, next) => {
   logger.info(`${req.method} ${req.url}`);
